3d6b098fe52df7cedc90e6186089530f64453e7c
Gahotx <gahotx@163.com>
2024-03-18 17:16:56 +0800

    feat: 首页新增轮播图、分类、热门推荐三个模块

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,55 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { getHomeBannerAPI, getHomeCategoryAPI, getHomeHotAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
+import CategoryPanel from './components/CategoryPanel.vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+import HotPanel from './components/HotPanel.vue'
+
+// 获取首页轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerList = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 获取首页分类数据
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryList = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
+// 获取首页热门推荐数据
+const hotList = ref<HotItem[]>([])
+const getHomeHotList = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerList()
+  getHomeCategoryList()
+  getHomeHotList()
+})
 </script>
 
 <template>
   <CustomNavbar />
+  <XtxSwiper :list="bannerList" />
+  <CategoryPanel :list="categoryList" />
+  <HotPanel :list="hotList" />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
+}
 </style>
---
+++ src/services/home.ts
@@ -0,0 +1,35 @@
+import { http } from '@/utils/request'
+
+/**
+ * 首页-广告区域-小程序
+ * @param distributionSite 广告区域展示位置 1 为首页（默认值）2 为商品分类页
+ */
+export function getHomeBannerAPI(distributionSite = 1) {
+  return http<BannerItem[]>({
+    method: 'GET',
+    url: '/home/banner',
+    data: {
+      distributionSite
+    }
+  })
+}
+
+/**
+ * 首页-前台分类-小程序
+ */
+export function getHomeCategoryAPI() {
+  return http<CategoryItem[]>({
+    method: 'GET',
+    url: '/home/category/mutli'
+  })
+}
+
+/**
+ * 首页-热门推荐-小程序
+ */
+export function getHomeHotAPI() {
+  return http<HotItem[]>({
+    method: 'GET',
+    url: '/home/hot/mutli'
+  })
+}
